f9d3faede9fb8eeaf9ee08ab362db7561ec2de21
Developer Software <muhamad.deva.arofi@gmail.com>
2020-08-03 20:38:29 +0700

    Update WelcomeController.php

--- app/controllers/WelcomeController.php
+++ app/controllers/WelcomeController.php
@@ -9,11 +9,6 @@ class WelcomeController extends dsController
         parent::__construct();
     }
 
-    public function sample($tes = '',$keep = '')
-    {
-        echo 'hello '. $tes. ' ' . $keep;
-    }
-
     public function index()
     {
         $data = array(
@@ -21,23 +16,6 @@ class WelcomeController extends dsController
             'welcomeText' => 'Let\'s make a better world!',
             'buttonText'  => 'Get Started'
         );
-        view("welcome.slice", $data);
-    }
-    public function other($nama = ''){
-        $data['nama'] = $nama .' '. Input::get('nama');
-        view('main', $data);
-    }
-    public function insert_batch()
-    {
-        BackEnd::insert('actor',[
-            [
-                'first_name'=>'deva',
-                'last_name'=>'arofi'
-            ],
-            [
-                'first_name'=>'arba',
-                'last_name'=>'karim'
-            ]
-        ]);
+        view("welcome.pie", $data);
     }
 }
